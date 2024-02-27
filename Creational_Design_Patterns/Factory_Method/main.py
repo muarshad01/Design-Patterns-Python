4eef2f39b98f30fdd955a35be5b93a183c3ae4ea
Muhammad Arshad <marshad@marshad-ltmnwup.internal.salesforce.com>
2024-02-27 16:10:33 -0500

    update

--- Creational_Design_Patterns/Factory_Method/main.py
+++ Creational_Design_Patterns/Factory_Method/main.py
@@ -3,7 +3,7 @@ from abc import ABC, abstractmethod
 
 
 class Creator(ABC):
-    
+
     @abstractmethod
     def factory_method(self):
         pass
@@ -13,7 +13,7 @@ class Creator(ABC):
         result = f"Creator: The same creator's code has just worked with {product.operation()}"
 
         return result
-    
+
 
 class ConcreteCreator1(Creator):
     def factory_method(self) -> Product:
@@ -24,13 +24,16 @@ class ConcreteCreator2(Creator):
     def factory_method(self) -> Product:
         return ConcreteProduct2()
 
+
 # --------------------------------------------------------------------
 
+
 class Product(ABC):
-    
+
     @abstractmethod
     def operation(self) -> str:
         pass
+
 
 class ConcreteProduct1(Product):
     def operation(self) -> str:
@@ -41,12 +44,18 @@ class ConcreteProduct2(Product):
     def operation(self) -> str:
         return "{Result of the ConcreteProduct2}"
 
+
 # --------------------------------------------------------------------
+
 
 def client_code(creator: Creator) -> None:
 
-    print(f"Client: I'm not aware of the creator's class, but it still works.\n"
-          f"{creator.some_operation()}", end="")
+    print(
+        f"Client: I'm not aware of the creator's class, but it still works.\n"
+        f"{creator.some_operation()}",
+        end="",
+    )
+
 
 # --------------------------------------------------------------------
 
