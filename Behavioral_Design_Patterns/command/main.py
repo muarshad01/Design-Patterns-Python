0499ff15de5a59373551754fc8cbbbf5cfd58c60
Muhammad Arshad <marshad@marshad-ltmnwup.internal.salesforce.com>
2024-03-03 19:41:06 -0500

    update

--- Behavioral_Design_Patterns/command/main.py
+++ Behavioral_Design_Patterns/command/main.py
@@ -7,8 +7,8 @@ class Command(ABC):
     The Command interface declares a method for executing a command.
     """
 
-        @abstractmethod
-        def execute(self) -> None:
+    @abstractmethod
+    def execute(self) -> None:
         pass
 
 
